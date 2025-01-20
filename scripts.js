3baafc7f37f8776a1dd7b09db744ca272e889cef
Pavekatal <pavekatal@gmail.com>
2025-01-20 20:41:57 +0800

    edits removed

--- scripts.js
+++ scripts.js
@@ -10,20 +10,3 @@ document.getElementById('anchor-link').addEventListener('click', function(event)
     }
 
 });
-
-// const runningBox = document.querySelector('.top__nav-box');
-
-// function runningFunc() {
-//     const totalWidth = runningBox.scrollWidth;
-//     const containerWidth = runningBox.clientWidth;
-//     let animationLine = (totalWidth / containerWidth) * 20; 
-//     runningBox.style.animationDuration = `${animationLine}s`; 
-// }
-
-// let resizeTimeout;
-// window.onresize = function() {
-//     clearTimeout(resizeTimeot);
-//     resizeTimeout = setTimeout(runningFunc, 200);
-// };
-
-// window.onload = runningFunc;
